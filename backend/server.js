03ed013ae0e11b450a1ca2a733962e19c596d7c8
uche269 <uchechukwunnorom2004@gmail.com>
2026-02-18 22:53:37 +0100

    Configure production deployment with remote DB

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ import { createTransport } from 'nodemailer';
 import multer from 'multer';
 import { parse } from 'csv-parse/sync';
 import { readFileSync } from 'fs';
-import pdfParse from 'pdf-parse';
+import pdfParse from 'pdf-parse/lib/pdf-parse.js';
 
 dotenv.config();
 
